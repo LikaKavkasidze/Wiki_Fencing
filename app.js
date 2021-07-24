054ec1f028fe7450c7ec7ac93756db3fda36fa5d
Stalone <talone@boxph.one>
2021-07-24 23:30:47 +0200

    Add auto-pooling

--- app.js
+++ app.js
@@ -18,6 +18,30 @@ const app = express();
 
 const PageEditor = pageEditorFactory(bot);
 
+// TODO: move this to a separate file
+// Add array comparaison possibility
+// see https://stackoverflow.com/questions/7837456/how-to-compare-arrays-in-javascript
+Array.prototype.equals = function(array) {
+	if(!Array.isArray(array)) { return false; }
+
+	for(var i = 0; i < this.length; i++) {
+		// Check if we have nested arrays
+		if(this[i] instanceof Array && array[i] instanceof Array) {
+			// Recurse into the nested arrays
+			if(!this[i].equals(array[i]))
+				return false;
+		} else if(this[i] != array[i]) {
+			// Warning - two different object instances will never be equal: {x:20} != {x:20}
+			return false;
+		}
+	}
+
+	return true;
+};
+
+// Hide method from for-in loops
+Object.defineProperty(Array.prototype, "equals", { enumerable: false });
+
 // Log the bot in!
 bot.logIn((e, data) => {
 	if(e) {
@@ -32,6 +56,13 @@ function updateEvent(event) {
 	fetch(event.originPage)
 		.then(res => res.text())
 		.then(res => {
+			// Prevent update too often
+			const timestamp = Date.now();
+
+			if(event.lastUpdate && event.lastUpdate + 5 * 60000 > timestamp) return;
+
+			event.lastUpdate = timestamp;
+
 			const scraped = scraper(res);
 			const editor = new PageEditor(event.wikiPage);
 
@@ -43,7 +74,11 @@ function updateEvent(event) {
 			delete scraped["t4"];
 			delete scraped["t2"];
 
+			let changed = false;
+
 			for(const table in scraped) {
+				if(event.previousTables && scraped[table].equals(event.previousTables[table])) continue;
+
 				const fmt = wikiFmt(scraped[table]);
 				
 				for(const [part, currentTable] of Object.entries(fmt)) {
@@ -55,11 +90,16 @@ function updateEvent(event) {
 				}
 
 				editor.addCommitMessage(table + ", ");
+				changed = true;
 			}
 
-			editor.commit = editor.commit.slice(0, -2);
-			editor.addCommitMessage(".");
-			editor.submitPage();
+			if(changed) {
+				editor.commit = editor.commit.slice(0, -2);
+				editor.addCommitMessage(".");
+				editor.submitPage();
+
+				event.previousTables = scraped;
+			}
 		})
 }
 
@@ -69,7 +109,7 @@ const requestHandler = scheduledEvent =>
 
 		if(timestamp > scheduledEvent.startTime && timestamp < scheduledEvent.endTime) {
 			updateEvent(scheduledEvent);
-			res.status(204).send();
+			res.send("Update done.");
 		} else {
 			res.status(403).send();
 		}
@@ -83,5 +123,14 @@ for(const scheduledEvent of config.schedule) {
 
 	app.get(`/${scheduledName}`, requestHandler(scheduledEvent));
 }
+
+function updateAll() {
+	for(const scheduledEvent of config.schedule) {
+		updateEvent(scheduledEvent);
+	}
+}
+
+// Auto-pooling every minute
+setInterval(updateAll, 60000);
 
 app.listen(3000);
